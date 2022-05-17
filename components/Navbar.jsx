eede7324bb78ef09f00d749876b54dcc747e558f
daxidngyn <daxidnguyen@gmail.com>
2022-05-17 12:14:04 -0700

    added shopify logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+
 const Navbar = () => {
   return (
-    <nav className="h-16 fixed bg-emerald-700 w-full text-white">
-      <div className="flex justify-between items-center h-full px-4 md:px-6">
-        <span className="text-xl font-bold">David Nguyen</span>
+    <nav className="h-16 fixed bg-emerald-700 w-full text-white z-50">
+      <div className="flex justify-center items-center h-full px-4 md:px-6 gap-x-4">
+        <Image src="/shopifylogo.png" width={40} height={40} />
+        <span className="text-2xl font-bold">
+          Fall 2022 Frontend Intern Challenge
+        </span>
       </div>
     </nav>
   );
